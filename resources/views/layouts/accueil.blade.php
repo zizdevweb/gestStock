720b39bd644d827ae2fbb68ca65127435502004a
fall <ziz17fall@gmail.com>
2020-01-07 15:04:08 +0000

    login module

--- resources/views/layouts/accueil.blade.php
+++ resources/views/layouts/accueil.blade.php
@@ -274,15 +274,15 @@
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="{{route('login')}}">
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
+                  Connexion
                 </a>
                 
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Deconnectez
+                  Deconnexion
                 </a>
               </div>
             </li>
