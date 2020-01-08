3711254f09d6e090d2fcc0fc29f5c9a3a53c7a46
amadou <amadougalimatech@gmail.com>
2020-01-08 08:31:44 +0000

    Add frontawesome to resource/js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,13 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
+require("@fortawesome/fontawesome-free/scss/brands.scss");
+require("@fortawesome/fontawesome-free/scss/fontawesome.scss");
+require("@fortawesome/fontawesome-free/scss/regular.scss");
+require("@fortawesome/fontawesome-free/scss/solid.scss");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +23,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +35,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app"
 });
